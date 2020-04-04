242203a71ff0d5ff98823ba9a4e59aa21512bd5f
Eugene Shangin <fessracha@gmail.com>
2020-04-04 19:48:36 +0500

    1

--- quiz-builder.js
+++ quiz-builder.js
@@ -5,20 +5,48 @@ class QuizBuilder {
         this.current_question = 0
         this.results = []
         this.time_left = 0
-        this.$container = options.$container
+        this.$container = options.$container,
+        this.questions = options.questions
     }
 
-    build() {
+    init() {
+        this.buildBase()
+    }
+
+    setHead(html) {
+        const $head = document.querySelector('.x-quiz__head')
+
+        $head.innerHTML(html)
+    }
+
+    setBody(html) {
+        const $body = document.querySelector('.x-quiz__body')
+
+        $body.innerHTML(html)
+    }
+
+    buildBase() {
         $quiz_container.innerHTML = `
             <div class="x-quiz">
-                <div class="x-quiz__title">
-                    Welcome ${this.name}
+                <div class="x-quiz__head">
+                    <div class="x-quiz__title">
+                        Welcome ${this.name}
+                    </div>
                 </div>
-                <div class="x-quiz__description">
-                    ${this.description}
+                <div class="x-quiz__body">
+                    <div class="x-quiz__description">
+                        ${this.description}
+                    </div>
+                    <div class="mt-4">
+                        <button class="btn btn-primary" id="x-quiz-start">Start</button>
+                    </div>
                 </div>
             <div/>
         `
+
+        const $start_btn = document.querySelector('#x-quiz-start')
+
+        $start_btn.addEventListener('click', this.start);
 
         console.log('Generate HTML Quiz')
     }
